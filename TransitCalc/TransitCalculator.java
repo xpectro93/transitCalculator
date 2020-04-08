77a17177398be368eed642eba6b61a0cc20df6c4
xpextro93 <jonathanandrade@pursuit.org>
2020-04-08 00:50:44 -0400

    add first test of Dictionary in files

--- TransitCalc/TransitCalculator.java
+++ TransitCalc/TransitCalculator.java
@@ -1,5 +1,34 @@
-class TransitCalculator {
+import java.util.*;
 
+public class TransitCalculator {
+
+    int totalDays;
+    int totalRides;
+    Dictionary metroCards = new Hashtable();
+    // constructor
+    public TransitCalculator (int days, int rides){
+        this.totalDays = days;
+        this.totalRides = rides;
+
+    }
+
+
+    // 7 day calc method
+    public double unlimited7Price () {
+        int passes = totalDays / 7;
+
+        if(passes % 7 > 0){
+            passes += 1;
+        }
+
+
+        
+        double cost = passes *  ;
+
+
+
+        return cost;
+    }
     public static void main(String[] args) {
         System.out.println("This is my first test");
     }
