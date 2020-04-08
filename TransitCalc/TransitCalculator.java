19e00fc2815295715a9d39f8a0d527677ae55e52
xpextro93 <jonathanandrade@pursuit.org>
2020-04-08 01:49:26 -0400

    clear console

--- TransitCalc/TransitCalculator.java
+++ TransitCalc/TransitCalculator.java
@@ -16,53 +16,52 @@ public class TransitCalculator {
     String cardName[] = {"One and Done", "Weekly Relief", "Raking in The monthly Benefits package"};
     //this should be like a JavaScript Object
     // constructor
-    public TransitCalculator (int days, int rides){
+    public TransitCalculator (final int days, final int rides) {
         this.totalDays = days;
         this.totalRides = rides;
 
     }
 
-
     // 7 day calc method
-    public double unlimited7Price () {
+    public double unlimited7Price() {
         int passes = totalDays / 7;
 
-        if(passes % 7 > 0){
+        if (passes % 7 > 0) {
             passes += 1;
-            System.out.println(passes);
         }
-        
-        //how much to buy
-        double cost = passes *  prices[1];
 
-        //calculate how much each ride would cost
+        // how much to buy
+        double cost = passes * prices[1];
+
+        // calculate how much each ride would cost
         cost = cost / totalRides;
-       
+
         return cost;
     }
-    public double[] getRidesPrices () {
-        double maxSingleRide = totalRides * prices[0];
-        double maxMonthRide = prices[2];
-        double[] ridesCost = { maxSingleRide, unlimited7Price(), maxMonthRide};
-        //this value will only show location of where the data is stored
+
+    public double[] getRidesPrices() {
+        final double maxSingleRide = totalRides * prices[0];
+        final double maxMonthRide = prices[2];
+        final double[] ridesCost = { maxSingleRide, unlimited7Price(), maxMonthRide };
+        // this value will only show location of where the data is stored
         return ridesCost;
     }
 
-    public String getBestFare () {
+    public String getBestFare() {
         int lowest = 0;
-        for(int i = 0; i < prices.length; i++){
+        for (int i = 0; i < prices.length; i++) {
             // System.out.print(prices[i]);
-            if(getRidesPrices()[i] < getRidesPrices()[lowest]){
+            if (getRidesPrices()[i] < getRidesPrices()[lowest]) {
                 lowest = i;
             }
         }
-        return "You should choose the "+ cardName[lowest] + " at "+getRidesPrices()[lowest] + " per ridarino.";
+        return "You should choose the " + cardName[lowest] + " at " + getRidesPrices()[lowest] + " per ridarino.";
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println("This is my first test");
 
-        TransitCalculator jake =  new TransitCalculator(22 , 50);
+        final TransitCalculator jake = new TransitCalculator(22, 50);
 
         System.out.println(jake.unlimited7Price());
 
